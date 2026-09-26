c31ab515c97f8be48c5a46cd9c391a11d6f89639
skagamamiri <sekolah-8965@moe-dl.edu.my>
2026-09-26 09:18:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smart-hub-v2';
+const CACHE = 'smart-hub-v3';
 const PRECACHE = ['./', './index.html', './manifest.json', './smart-hub-logo.png'];
 
 self.addEventListener('install', event => {
